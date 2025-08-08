642165359befab2929ee519e5ca6ac259cea0979
Amit <amitak985059@gmail.com>
2025-08-09 03:19:55 +0530

    code updated

--- main.cpp
+++ main.cpp
@@ -1,62 +1,43 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-class DisjointSet
-{
+class DisjointSet {
     vector<int> rank, parent, size;
-    DisjointSet(int n)
-    {
+public:
+    DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
-        for (int i = 0; i < n + 1; i++)
-        {
+        size.resize(n + 1, 1); // FIX: initialize size array
+        for (int i = 0; i < n + 1; i++) {
             parent[i] = i;
-            size[i] = 1;
         }
     }
-    int findUParent(int node)
-    {
-        if (parent[node] != node)
-        {
+    int findUParent(int node) {
+        if (parent[node] != node) {
             parent[node] = findUParent(parent[node]);
         }
         return parent[node];
     }
 
-    void unionByRank(int u, int v)
-    {
+    void unionByRank(int u, int v) {
         int uParent = findUParent(u);
         int vParent = findUParent(v);
-        if (uParent == vParent)
-            return;
-        if (rank[uParent] < rank[vParent])
-        {
+        if (uParent == vParent) return;
+        if (rank[uParent] < rank[vParent]) {
             parent[uParent] = vParent;
-        }
-        else if (rank[uParent] > rank[vParent])
-        {
+        } else if (rank[uParent] > rank[vParent]) {
             parent[vParent] = uParent;
-        }
-        else
-        {
+        } else {
             parent[vParent] = uParent;
             rank[uParent]++;
         }
     }
 
-    void unionBySize(int u, int v)
-    {
+    void unionBySize(int u, int v) {
         int ulp_u = findUParent(u);
         int ulp_v = findUParent(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (size[ulp_u] < size[ulp_v])
-        {
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
             parent[ulp_u] = ulp_v;
             size[ulp_v] += size[ulp_u];
-        }
-        else
-        {
+        } else {
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
